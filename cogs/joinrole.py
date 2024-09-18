62f1a822290e2233c2aa7a98bea59bb5bc246829
episkop <120209125+xpsixoz@users.noreply.github.com>
2024-09-18 07:04:34 +0300

    Add files via upload

--- cogs/joinrole.py
+++ cogs/joinrole.py
@@ -1,7 +1,7 @@
 import disnake
 from disnake.ext import commands
 import sqlite3
-from config import DATABASE, localban_role
+from config import DATABASE, localban_role, noverify_role  # Добавьте unverify_role в импорт
 
 class JoinRoles(commands.Cog):
     def __init__(self, bot):
@@ -11,20 +11,26 @@ class JoinRoles(commands.Cog):
     async def on_member_join(self, member):
         conn = sqlite3.connect(DATABASE)
         cursor = conn.cursor()
-        
         cursor.execute("SELECT left FROM verified WHERE user_id = ?", (member.id,))
         result = cursor.fetchone()
-        
         if result and result[0] >= 5:
-            roles_to_remove = [role for role in member.roles if role.id != disnake.Object(id=member.guild.id).id]  # Исключаем роль @everyone
+            # Исключаем роль @everyone
+            everyone_role = member.guild.id
+            roles_to_remove = [role for role in member.roles if role.id != everyone_role]
             await member.remove_roles(*roles_to_remove, reason="user left 5 times")
             
             ban_role = disnake.utils.get(member.guild.roles, id=localban_role)
             if ban_role:
                 await member.add_roles(ban_role, reason="user left 5 times")
-
+        else:
+            # Добавление роли unverify_role при заходе на сервер
+            unverify_role_obj = disnake.utils.get(member.guild.roles, id=noverify_role)
+            if unverify_role_obj:
+                await member.add_roles(unverify_role_obj, reason="Assigned unverified role on join")
+        
+        
         conn.close()
 
 def setup(bot):
     bot.add_cog(JoinRoles(bot))
-    print("joinroles loaded!")
+    print("JoinRoles loaded!")
